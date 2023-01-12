e103b44dc09d6779202170c0878fe9e6a3f1d688
BigSmoke010 <behirimohamedcherif0@gmail.com>
2023-01-12 23:09:34 +0100

    add red flash when damaged and convert char class to sprite

--- characters.py
+++ characters.py
@@ -1,41 +1,48 @@
 import pygame
 
-class character:
+class character(pygame.sprite.Sprite):
     def __init__(self, playernum, playerpic=None) -> None:
+        super().__init__()
+        self.playernum = playernum
         self.char1imgs = [pygame.image.load('./images/Player/r_run_1.png').convert_alpha(),pygame.image.load('./images/Player/r_run_2.png').convert_alpha(),pygame.image.load('./images/Player/r_run_3.png').convert_alpha(),pygame.image.load('./images/Player/r_run_4.png').convert_alpha(),pygame.image.load('./images/Player/r_run_5.png').convert_alpha(),pygame.image.load('./images/Player/r_run_6.png').convert_alpha(),pygame.image.load('./images/Player/r_run_7.png').convert_alpha(),pygame.image.load('./images/Player/r_run_8.png').convert_alpha(),pygame.image.load('./images/Player/r_run_9.png').convert_alpha(),pygame.image.load('./images/Player/r_run_10.png').convert_alpha(),pygame.image.load('./images/Player/r_run_11.png').convert_alpha(),pygame.image.load('./images/Player/r_run_12.png').convert_alpha(),pygame.image.load('./images/Player/r_run_13.png').convert_alpha(),pygame.image.load('./images/Player/r_run_14.png').convert_alpha(),pygame.image.load('./images/Player/r_run_15.png').convert_alpha(),pygame.image.load('./images/Player/r_run_16.png').convert_alpha(),]
         self.char1idleimgs = [pygame.image.load('./images/Player/r_idle_1.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_2.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_3.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_4.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_5.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_6.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_7.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_8.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_9.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_10.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_11.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_12.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_13.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_14.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_15.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_16.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_17.png').convert_alpha()]
         self.bulletimg = pygame.image.load('./images/bullet.png').convert_alpha()
         self.indx = 0
-        self.chosenimg = self.char1imgs[self.indx]
+        self.image = self.char1imgs[self.indx]
 
         if playernum == 1:
-            self.char = self.chosenimg.get_rect(topleft=[100,1])
+            self.rect = self.image.get_rect(topleft=[100,1])
         elif playernum == 2:
-            self.char = self.chosenimg.get_rect(topleft=[600,1])
+            self.rect = self.image.get_rect(topleft=[600,1])
 
         self.bullets = []
         self.tmplist = []
 
-    def showchar(self, screen, direction, isstanding):
+    def update(self, screen, direction1, direction2, isstanding1, isstanding2):
+        if self.playernum == 1:
+            direction = direction1
+            isstanding = isstanding1
+        else:
+            direction = direction2
+            isstanding = isstanding2
+
         if not isstanding:
             if direction == 'right' or direction == 'left':
                 for i in self.char1imgs:
                     self.indx += 0.01
                     if self.indx >= len(self.char1imgs):
                         self.indx = 0
-                    self.chosenimg = self.char1imgs[int(self.indx)]
+                    self.image= self.char1imgs[int(self.indx)]
                     if direction == 'left':
-                        self.chosenimg = pygame.transform.flip(self.chosenimg, True, False)
-                    screen.blit(self.chosenimg, self.char)
+                        self.image = pygame.transform.flip(self.image, True, False)
         if isstanding:
             for i in self.char1idleimgs:
                 self.indx += 0.01
                 if self.indx >= len(self.char1idleimgs):
                     self.indx = 0
-                self.chosenimg = self.char1idleimgs[int(self.indx)]
+                self.image= self.char1idleimgs[int(self.indx)]
                 if direction == 'left':
-                    self.chosenimg = pygame.transform.flip(self.chosenimg, True, False)
-                screen.blit(self.chosenimg, self.char)
+                    self.image= pygame.transform.flip(self.image, True, False)
 
     def shoot(self, screen , chardir):
         for rect, dir in self.bullets:
@@ -59,7 +66,7 @@ class character:
     def shootdetach(self, x, y, shoted):
         if shoted:
             try:
-                self.bullets[-1][0].y = y
+                self.bullets[-1][0].y = y + 10
             except IndexError:
                 pass
 
--- mainloop.py
+++ mainloop.py
@@ -4,6 +4,7 @@ import map
 
 class loop:
     def __init__(self) -> None:
+        pygame.init()
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((800,600))
         self.map = map.pyMap(self.screen)
@@ -27,6 +28,9 @@ class loop:
         self.plyr2health = 100
         self.healthbar1 = pygame.rect.Rect(10, 50, self.plyr1health * 2, 15)
         self.healthbar2 = pygame.rect.Rect(550, 50, self.plyr2health * 2, 15)
+        self.charsgroup = pygame.sprite.Group()
+        self.charsgroup.add(self.char1)
+        self.charsgroup.add(self.char2)
         self.running = True
 
     def startloop(self):
@@ -50,11 +54,11 @@ class loop:
                         self.char2state = 'right'
                     if event.key == pygame.K_DOWN:
                         self.shot2 = True
-                        self.x = self.char2.char.x
-                        self.y = self.char2.char.y
+                        self.x = self.char2.rect.x
+                        self.y = self.char2.rect.y + 10
                         self.char2.bullets.append((pygame.rect.Rect(self.x, self.y, 25, 25), self.char2state))
                         self.char2.shoot(self.screen, self.char2state)
-                        self.char2.shootdetach(self.char2.char.x, self.char2.char.y, self.shot2)
+                        self.char2.shootdetach(self.char2.rect.x, self.char2.rect.y, self.shot2)
 
                     if event.key == pygame.K_w and self.alljumpsforchar1 < 2:
                         self.gravityforchar1 = -20
@@ -69,11 +73,11 @@ class loop:
                         self.char1state = 'left'
                     if event.key == pygame.K_s:
                         self.shot1 = True
-                        self.x = self.char1.char.x
-                        self.y = self.char1.char.y
+                        self.x = self.char1.rect.x
+                        self.y = self.char1.rect.y + 10
                         self.char1.bullets.append((pygame.rect.Rect(self.x, self.y, 25, 25), self.char1state))
                         self.char1.shoot(self.screen, self.char1state)
-                        self.char1.shootdetach(self.char1.char.x, self.char1.char.y, self.shot1)
+                        self.char1.shootdetach(self.char1.rect.x, self.char1.rect.y, self.shot1)
 
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_d:
@@ -94,7 +98,6 @@ class loop:
                             self.char2stand = True
                             self.xforchar2 = 0
 
-
             self.map.showmap(self.screen)
 
             pygame.draw.rect(self.screen, 'red', self.healthbar1)
@@ -102,18 +105,16 @@ class loop:
             self.healthbar1.width = self.plyr1health * 2
             self.healthbar2.width = self.plyr2health * 2
 
-            self.char1.showchar(self.screen, self.char1state, self.char1stand)    
-            self.char2.showchar(self.screen, self.char2state, self.char2stand)    
-
-            self.char1.char.x += self.xforchar1
-            self.char2.char.x += self.xforchar2
+            self.char1.rect.x += self.xforchar1
+            self.char2.rect.x += self.xforchar2
 
             self.gravityforchar2 += 1
             self.gravityforchar1 += 1
 
-            self.char1.char.y += self.gravityforchar1
-            self.char2.char.y += self.gravityforchar2
+            self.char1.rect.y += self.gravityforchar1
+            self.char2.rect.y += self.gravityforchar2
 
+            self.charsgroup.draw(self.screen)
             self.char1.shoot(self.screen, self.char1state)
             self.char2.shoot(self.screen, self.char2state)
 
@@ -122,7 +123,8 @@ class loop:
                     if bullet.colliderect(block):
                         self.char1.bullets.pop(self.char1.tmplist.index(bullet))
                         self.char1.tmplist.remove(bullet)
-                    if bullet.colliderect(self.char2.char):
+                    if bullet.colliderect(self.char2.rect):
+                        self.screen.blit(pygame.image.load('./images/Player/r_run_2_damaged.png').convert_alpha(), self.char2.rect)
                         self.char1.bullets.pop(self.char1.tmplist.index(bullet))
                         self.char1.tmplist.remove(bullet)
                         self.plyr2health -= 10
@@ -130,38 +132,40 @@ class loop:
                     if bullet.colliderect(block):
                         self.char2.bullets.pop(self.char2.tmplist.index(bullet))
                         self.char2.tmplist.remove(bullet)
-                    if bullet.colliderect(self.char1.char):
+                    if bullet.colliderect(self.char1.rect):
+                        self.screen.blit(pygame.image.load('./images/Player/r_run_2_damaged.png').convert_alpha(), self.char1.rect)
                         self.char2.bullets.pop(self.char2.tmplist.index(bullet))
                         self.char2.tmplist.remove(bullet)
                         self.plyr1health -= 10
                         print(self.plyr1health)
                         print('ouuchh')
                 for i in range(block.left, block.right):
-                    if self.char1.char.collidepoint(i, block.top):
-                        self.char1.char.bottom = block.top
+                    if self.char1.rect.collidepoint(i, block.top):
+                        self.char1.rect.bottom = block.top
                         self.gravityforchar1 = 0
                         self.alljumpsforchar1 = 0
-                    if self.char1.char.collidepoint(i, block.bottom):
+                    if self.char1.rect.collidepoint(i, block.bottom):
                         self.gravityforchar1 = 0
-                        self.char1.char.top = block.bottom
-                    if self.char2.char.collidepoint(i, block.top):
-                        self.char2.char.bottom = block.top
+                        self.char1.rect.top = block.bottom
+                    if self.char2.rect.collidepoint(i, block.top):
+                        self.char2.rect.bottom = block.top
                         self.gravityforchar2 = 0
                         self.alljumpsforchar2 = 0
-                    if self.char2.char.collidepoint(i, block.bottom):
+                    if self.char2.rect.collidepoint(i, block.bottom):
                         self.gravityforchar2 = 0
-                        self.char2.char.top = block.bottom
+                        self.char2.rect.top = block.bottom
                 for i in range(block.top, block.bottom):
-                    if self.char1.char.collidepoint(block.left, i):
-                        self.char1.char.right = block.left
-                    if self.char1.char.collidepoint(block.right, i):
-                        self.char1.char.left = block.right
-                    if self.char2.char.collidepoint(block.left, i):
-                        self.char2.char.right = block.left
-                    if self.char2.char.collidepoint(block.right, i):
-                        self.char2.char.left = block.right
+                    if self.char1.rect.collidepoint(block.left, i):
+                        self.char1.rect.right = block.left
+                    if self.char1.rect.collidepoint(block.right, i):
+                        self.char1.rect.left = block.right
+                    if self.char2.rect.collidepoint(block.left, i):
+                        self.char2.rect.right = block.left
+                    if self.char2.rect.collidepoint(block.right, i):
+                        self.char2.rect.left = block.right
                         
-            pygame.display.update()
+            self.charsgroup.update(self.screen, self.char1state, self.char2state, self.char1stand, self.char2stand)
+            pygame.display.flip()
             self.clock.tick(60)
 
 
