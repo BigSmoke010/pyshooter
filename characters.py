3fbfb5341632d3e442240bfb0c5028894d0794e7
BigSmoke010 <behirimohamedcherif0@gmail.com>
2023-01-14 20:02:23 +0100

    add gameover screen and death animation

--- characters.py
+++ characters.py
@@ -8,15 +8,19 @@ class character(pygame.sprite.Sprite):
         self.playernum = playernum
         self.char1imgs = [pygame.image.load('./images/Player/r_run_1.png').convert_alpha(),pygame.image.load('./images/Player/r_run_2.png').convert_alpha(),pygame.image.load('./images/Player/r_run_3.png').convert_alpha(),pygame.image.load('./images/Player/r_run_4.png').convert_alpha(),pygame.image.load('./images/Player/r_run_5.png').convert_alpha(),pygame.image.load('./images/Player/r_run_6.png').convert_alpha(),pygame.image.load('./images/Player/r_run_7.png').convert_alpha(),pygame.image.load('./images/Player/r_run_8.png').convert_alpha(),pygame.image.load('./images/Player/r_run_9.png').convert_alpha(),pygame.image.load('./images/Player/r_run_10.png').convert_alpha(),pygame.image.load('./images/Player/r_run_11.png').convert_alpha(),pygame.image.load('./images/Player/r_run_12.png').convert_alpha(),pygame.image.load('./images/Player/r_run_13.png').convert_alpha(),pygame.image.load('./images/Player/r_run_14.png').convert_alpha(),pygame.image.load('./images/Player/r_run_15.png').convert_alpha(),pygame.image.load('./images/Player/r_run_16.png').convert_alpha(),]
         self.char1idleimgs = [pygame.image.load('./images/Player/r_idle_1.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_2.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_3.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_4.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_5.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_6.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_7.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_8.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_9.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_10.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_11.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_12.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_13.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_14.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_15.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_16.png').convert_alpha(),pygame.image.load('./images/Player/r_idle_17.png').convert_alpha()]
+        self.char1deathimgs = [pygame.image.load('./images/Player/r_death_1.png').convert_alpha(),pygame.image.load('./images/Player/r_death_2.png').convert_alpha(),pygame.image.load('./images/Player/r_death_3.png').convert_alpha(),pygame.image.load('./images/Player/r_death_4.png').convert_alpha(),pygame.image.load('./images/Player/r_death_5.png').convert_alpha(),pygame.image.load('./images/Player/r_death_6.png').convert_alpha(),pygame.image.load('./images/Player/r_death_7.png').convert_alpha(),pygame.image.load('./images/Player/r_death_8.png').convert_alpha(),pygame.image.load('./images/Player/r_death_9.png').convert_alpha(),pygame.image.load('./images/Player/r_death_10.png').convert_alpha(),pygame.image.load('./images/Player/r_death_11.png').convert_alpha(),pygame.image.load('./images/Player/r_death_12.png').convert_alpha(),pygame.image.load('./images/Player/r_death_13.png').convert_alpha(),pygame.image.load('./images/Player/r_death_14.png').convert_alpha(),pygame.image.load('./images/Player/r_death_15.png').convert_alpha(),pygame.image.load('./images/Player/r_death_16.png').convert_alpha()]
         self.char2imgs = []
         self.char2idleimgs = []
         self.char2bullets = []
+        self.char2death = []
         for i in natsorted(os.listdir('./images/skeleton/walk/right/')):
             self.char2imgs.append(pygame.image.load('./images/skeleton/walk/right/'+ i).convert_alpha())
         for i in natsorted(os.listdir('./images/skeleton/idle/right/')):
             self.char2idleimgs.append(pygame.image.load('./images/skeleton/idle/right/' + i).convert_alpha())
         for i in natsorted(os.listdir('./images/skeleton/bone/')):
             self.char2bullets.append(pygame.image.load('./images/skeleton/bone/' + i).convert_alpha())
+        for i in natsorted(os.listdir('./images/skeleton/die/right/')):
+            self.char2death.append(pygame.image.load('./images/skeleton/die/right/' + i).convert_alpha())
         self.bulletimg = pygame.image.load('./images/bullet.png').convert_alpha()
         self.indx = 0
         self.bulletindex = 0
@@ -25,8 +29,6 @@ class character(pygame.sprite.Sprite):
             self.image = self.char1imgs[self.indx]
         elif playernum == 2:
             self.image = self.char2imgs[self.indx]
-
-
         if playernum == 1:
             self.rect = self.image.get_rect(topleft=[100,1])
         elif playernum == 2:
@@ -108,4 +110,26 @@ class character(pygame.sprite.Sprite):
                 self.bullets[-1][0].y = y + 10
             except IndexError:
                 pass
+
+    def death(self, num, screen, game):
+        if game == 2:
+            self.bullets.clear()
+            self.tmplist.clear()
+            if num == 2:
+                for i in self.char2death:
+                    self.indx += 0.01
+                    if self.indx >= len(self.char2death):
+                        break
+                    self.image = self.char2death[int(self.indx)]
+                    screen.blit(self.image, self.rect)
+            else:
+                for i in self.char1deathimgs:
+                    self.indx += 0.01
+                    if self.indx >= len(self.char1deathimgs):
+                        break
+                    self.image = self.char1deathimgs[int(self.indx)]
+                    screen.blit(self.image, self.rect)
+
+            
+
 
